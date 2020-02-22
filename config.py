e3ac557a10ac96ba99a62df32ce6b506d296f634
Clydeside <lipatov.artem@mail.ru>
2020-02-22 21:13:27 +0300

    Changing config

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '7465878a1299fd185ce1f77c1ef6364d'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
