cdbbac2b2a648de0cae6397e9804f8ce8769b610
Clydeside <lipatov.artem@mail.ru>
2020-02-26 22:57:40 +0300

    Adding validation

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 class User(db.Model):
@@ -20,3 +22,9 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
